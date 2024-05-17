a8c5830fbf9a88b809259b0d20e25a97ed947ae4
XimenaMorera <moreraximena@36@gmail.com>
2024-05-16 21:02:01 -0500

    Edit parte

--- Models/Cita.cs
+++ Models/Cita.cs
@@ -8,7 +8,6 @@ namespace AutoShopManager.Models
 
         [Column(TypeName = "date")]
         public DateTime Fecha { get; set; }
-
         public string Hora { get; set; }
         public string Descripcion { get; set; }
         public Cliente? Cliente { get; set; } = default!;//propiedad de navegacion
